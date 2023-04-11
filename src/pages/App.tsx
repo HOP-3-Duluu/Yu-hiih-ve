7eb47f777e77ef80c28e9a36ef79e7c877746e73
23-HOP3-Tsogt <Tsogt@Themuulns.local>
2023-04-11 18:27:34 +0800

    Created react native app. Set up folder structure.

---
+++ src/pages/App.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+type SectionProps = PropsWithChildren<{
+  title: string;
+}>;
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <Text>Hello, Yu hiimeer baina?</Text>
+    </SafeAreaView>
+  );
+}
+
+
+export default App;
