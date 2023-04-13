a5a33f38a58a440f31df0df10221cc349fe4775c
HOP3 Tuguldur Hero <TuguldurHero@NEST-96s-iMac.local>
2023-04-13 19:27:06 +0800

    categories section

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,42 +1,12 @@
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {SafeAreaView, Text} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-import { Header } from '../components/common/header';
-import { SearchInput } from '../components/core/searchinput';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
+const App = () => { 
   return (
     <SafeAreaView>
-      <Header/>
-      <SearchInput/>
+      
     </SafeAreaView>
   );
 }
-
 
 export default App;
