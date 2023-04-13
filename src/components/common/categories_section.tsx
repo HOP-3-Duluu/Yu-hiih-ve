9bd4bc196d3c1708d29dba0152f4f1af2eb6e66e
HOP3 Tuguldur Hero <TuguldurHero@NEST-96s-iMac.local>
2023-04-13 19:27:06 +0800

    categories section

---
+++ src/components/common/categories_section.tsx
@@ -0,0 +1,108 @@
+import {
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  Image,
+  FlatList,
+} from 'react-native';
+export const Categories = () => {
+  const mock_data = [
+    {
+      image_url: require('../../assets/image/nature.png'),
+      name: 'Nature',
+    },
+    {
+      image_url: require('../../assets/image/nature.png'),
+      name: 'PC',
+    },
+    {
+      image_url: require('../../assets/image/nature.png'),
+      name: 'Restaurant',
+    },
+    {
+      image_url: require('../../assets/image/nature.png'),
+      name: 'Pool',
+    },
+    {
+      image_url: require('../../assets/image/nature.png'),
+      name: 'Billiard',
+    },
+    {
+      image_url: require('../../assets/image/nature.png'),
+      name: 'Movie Theater',
+    },
+  ];
+
+  return (
+    <View style={styled.body}>
+      <View style={styled.container1}>
+        <Text
+          style={{
+            marginBottom: 10,
+            color: '#242424',
+            fontWeight: '500',
+            fontSize: 20,
+          }}>
+          Category
+        </Text>
+       <Pressable>
+       <Text
+          style={{
+            fontWeight: '400',
+            color: '#242424',
+            opacity: 0.5,
+            fontSize: 12,
+          }}>
+          See more categories
+        </Text>
+       </Pressable>
+      </View>
+      <FlatList
+        data={mock_data}
+        horizontal={true}
+        renderItem={({item}) => (
+          <View style={styled.container}>
+            <Pressable style={styled.button}>
+              <Image style={{width: 40, height: 40}} source={item.image_url} />
+              <Text style={{fontSize: 18, fontWeight: '400', marginLeft: 15}}>
+                {item.name}
+              </Text>
+            </Pressable>
+          </View>
+        )}
+      />
+    </View>
+  );
+};
+
+const styled = StyleSheet.create({
+  body: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+  },
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  container1: {
+    width: "90%",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  button: {
+    height: 60,
+    borderRadius: 10,
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    paddingLeft: 20,
+    paddingRight: 30,
+    marginRight: 21,
+  },
+});
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,40 +1,14 @@
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {SafeAreaView, Text} from 'react-native';
+import { Categories } from '../components/common/categories_section';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
+const App = () => { 
   return (
-    <SafeAreaView style={backgroundStyle}>
+    <SafeAreaView>
       <Text>Hello, Yu hiimeer baina?</Text>
+      <Categories/>
     </SafeAreaView>
   );
 }
-
 
 export default App;
