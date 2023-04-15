d6e1c174661452c9b984e13aa32aff565f248039
HOP3 Tuguldur Hero <TuguldurHero@NEST-96s-iMac.local>
2023-04-15 12:30:19 +0800

    navigations

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,7 +3,7 @@ import {SafeAreaView, Text} from 'react-native';
 import { Categories } from '../components/common/categories_section';
 import { Header } from '../components/common/header';
 
-export const Home = () => { 
+export const HomePage = () => { 
   return (
     <SafeAreaView>
       <Header/>
