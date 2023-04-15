72c370677d9fb81a2a8ff7fe459608789ae191b2
HOP3 Tuguldur Hero <TuguldurHero@NEST-96s-iMac.local>
2023-04-15 12:16:26 +0800

    navigation section

---
+++ src/pages/settings.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { SafeAreaView, View } from "react-native";
+
+export const Settings = () => {
+    return (
+        <SafeAreaView>
+
+        </SafeAreaView>
+    )
+}
