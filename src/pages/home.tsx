4d4dd602b7c8786c2438b71d90d381c2a40051e2
Hop3 Bilguun <h3-Bekku@Mandakh.local>
2023-04-20 18:40:26 +0800

    popular section

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import {SafeAreaView, Text} from 'react-native';
 import { Categories } from '../components/common/categories_section';
 import { Header } from '../components/common/header';
+import { Popular_section } from '../components/common/popular_section';
 
 export const HomePage = () => { 
   return (
     <SafeAreaView>
       <Header/>
       <Categories/>
+      <Popular_section/>
     </SafeAreaView>
   );
 }
