904d873809d41ec02084d4871caf09b33b358e83
HOP3 Tuguldur Hero <TuguldurHero@NEST-96s-iMac.local>
2023-04-25 18:30:21 +0800

    useContext v2

--- src/context/DataContext.tsx
+++ src/context/DataContext.tsx
@@ -1,16 +1,14 @@
-import {createContext, useContext, useState} from 'react';
+import {createContext, useContext} from 'react';
 
 export const DataType = createContext({
-  popular: []
-})
+  popular: [],
+});
 
 export const DataContextProvider = ({children}: any) => {
   const popular: never[] = [];
 
-  <DataType.Provider value={{popular}}>
-    {children}
-  </DataType.Provider>
-} 
+  return <DataType.Provider value={{popular}}>{children}</DataType.Provider>;
+};
 
 export const useDataContext = () => {
   return useContext(DataType);
--- src/navigations/StackNavigator.tsx
+++ src/navigations/StackNavigator.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import { HomePage } from '../pages/home';
 import {BottomTabNavigation} from './RootNavigator';
 import { CategoriesPage } from '../pages/categories';
 import { LocationPage } from '../pages/location';
 
 const Stack = createNativeStackNavigator();
 
-export const StackNavigator = ({navigation}: {navigation: any}) => {
+export const StackNavigator = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
