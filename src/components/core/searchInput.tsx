76e3d48c64abb0c30b5a200c2fc40d0e06352e65
Hop3-Oyunbaatar <Oba@NEST-94s-iMac.local>
2023-04-29 11:57:07 +0800

    Input zasav

--- src/components/core/searchInput.tsx
+++ src/components/core/searchInput.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Pressable, SafeAreaView, StyleSheet, TextInput, View } from 'react-native';
 import { SearchIcon } from '../../assets/icon';
+import { cyan } from '@mui/material/colors';
 
 export const SearchInput = ({ navigation }: any) => {
 
@@ -10,12 +11,15 @@ export const SearchInput = ({ navigation }: any) => {
             <View style={styles.body}>
                 <View>
                     <TextInput
+                        style={styles.input}
                         placeholder='Enter category'
                     />
                 </View>
-                <Pressable onPress={() => {
+                <Pressable
+                    style={styles.button}
+                    onPress={() => {
 
-                }}>
+                    }}>
                     <SearchIcon />
                 </Pressable>
             </View>
@@ -30,16 +34,28 @@ const styles = StyleSheet.create({
         width: 340,
         display: "flex",
         flexDirection: 'row',
-        borderRadius: 15
+        borderRadius: 15,
+        overflow: 'hidden'
     },
     body: {
         width: 340,
         display: "flex",
         flexDirection: 'row',
         justifyContent: 'space-between',
-        padding: 15,
+        paddingRight: 15,
         borderColor: 'grey',
         borderWidth: 0.3,
         borderRadius: 15,
+    }, 
+    input: {
+        width: 290,
+        paddingTop: 15,
+        paddingBottom: 15,
+        paddingLeft: 15
+    },
+    button: {
+        width: 50,
+        justifyContent: 'center',
+        alignItems: 'center'
     },
 });
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {SafeAreaView, Text} from 'react-native';
 import { Categories } from '../components/common/categories_section';
 import { Header } from '../components/common/header';
+import { SearchInput } from '../components/core/searchInput';
 
 export const HomePage = () => { 
   return (
